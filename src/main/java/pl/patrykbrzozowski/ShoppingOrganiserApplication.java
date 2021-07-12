71717733577877689f1c811fe7f3a49191ad85ac
patryk-brzozowski <patryk.brzozowski@interia.pl>
2021-07-12 22:08:54 +0200

    Basic configuration

---
+++ src/main/java/pl/patrykbrzozowski/ShoppingOrganiserApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykbrzozowski;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingOrganiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingOrganiserApplication.class, args);
+	}
+
+}
